a373de678cbaaf13f21bdcf199f670c841756dd4
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2024-10-14 10:23:48 +0200

    Add files via upload

---
+++ DWES/Ejercicios PHP/EjercicioBásico P.O(2)/visualizar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once 'DiasAño.php';
+    $fecha = explode('/',$_POST['fecha']);
+    $dia = $fecha[0];
+    $mes = $fecha[1];
+    $año = $fecha[2];
+
+    $objDiasAño = new DiasAño($año);
+    // print_r($objDiasAño->mesesDias);
+
+    echo 'La fecha introducida es: '.$dia.' de '.$objDiasAño->getMesesDias($mes)['mes'].' de '.$año.'
+                <br> Febrero tiene: '.$objDiasAño->getMesesDias(2)['dias'].' dias';
