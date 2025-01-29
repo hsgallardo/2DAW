8c576e42e65743273687d3abb89be3eb30cbdee7
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-01-29 20:38:27 +0100

    Delete DWES/Ejercicios PHP/instalacion hugo/Instalacion directory

--- DWES/Ejercicios PHP/instalacion hugo/Instalacion/instalacion_admin/config/configdb.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("SERVIDOR", "localhost"); // DATOS SERVIDOR
-    define("USUARIO", "hugo_evg");  // DATOS USUARIO BBDD
-    define("PASSWORD", "1234"); // DATOS CONTRASEÑA BBDD
-    define("BBDD", "applibros"); // NOMBRE BBDD
-?>
