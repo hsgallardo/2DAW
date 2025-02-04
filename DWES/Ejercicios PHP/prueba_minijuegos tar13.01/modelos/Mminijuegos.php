808d572198992b64e0bc398c4a3767fdb7b8b72e
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-02-04 10:06:28 +0100

    Add files via upload

---
+++ DWES/Ejercicios PHP/prueba_minijuegos tar13.01/modelos/Mminijuegos.php
@@ -0,0 +1,38 @@
+<?php
+class Mminijuegos {
+    private $conexion;
+
+    public function __construct(){
+        require_once 'configDb.php';
+        $this->conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); 
+        $this->conexion->set_charset("utf8"); 
+    }
+
+    // Obtener todos los ámbitos
+    public function obtenerAmbitos() {
+        $query = "SELECT idambito, nombre FROM ambito";
+        $resultado = $this->conexion->query($query);
+        $ambitos = [];
+        while ($fila = $resultado->fetch_assoc()) {
+            $ambitos[] = $fila;
+        }
+        return $ambitos;
+    }
+
+    public function obtenerMinijuegosPorAmbito($idsAmbitos) {
+        $idsAmbitos = implode(',', $idsAmbitos); // Convertir array en cadena
+        $query = "SELECT m.nombre AS minijuego, m.url_juego, a.nombre AS ambito 
+                  FROM minijuegos m
+                  JOIN ambito a ON m.idambito = a.idambito
+                  WHERE m.idambito IN ($idsAmbitos)";
+        
+        $resultado = $this->conexion->query($query);
+        $minijuegos = [];
+        while ($fila = $resultado->fetch_assoc()) {
+            $minijuegos[] = $fila;
+        }
+        return $minijuegos;
+    }
+    
+}
+?>
