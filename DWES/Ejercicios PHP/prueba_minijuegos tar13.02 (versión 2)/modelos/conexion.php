fb126acd42b97f60575098052c03c80948800f73
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-02-05 13:51:33 +0100

    Delete DWES/Ejercicios PHP/prueba_minijuegos tar13.02 (versión 2) directory

--- DWES/Ejercicios PHP/prueba_minijuegos tar13.02 (versión 2)/modelos/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'configDb.php'; 
-
-    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); 
-    $conexion->set_charset("utf8"); 
-    $controlador = new mysqli_driver();
-    $controlador->report_mode = MYSQLI_REPORT_OFF;
-    $texto_error=$conexion->errno;
-?>
