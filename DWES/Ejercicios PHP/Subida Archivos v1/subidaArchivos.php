1789de4f80a900f09bf24d34ff583baa1abf7bde
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-03-08 16:42:33 +0100

    Delete DWES/Ejercicios PHP/Subida Archivos v1 directory

--- DWES/Ejercicios PHP/Subida Archivos v1/subidaArchivos.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(move_uploaded_file($_FILES['archivo']['tmp_name'], 'imagenes/' .$_FILES['archivo']['name'])){
-    echo 'Archivo subido correctamente';
-}else{
-    echo 'Error en la subida de los archivos';
-}
-
-?>
