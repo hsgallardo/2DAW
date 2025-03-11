f99c18992aa07077bd7c1fad3c43ac77784351b4
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-03-11 10:49:29 +0100

    Delete DWES/Ejercicios PHP/Examen Corregido/formulario_alta.php

--- DWES/Ejercicios PHP/Examen Corregido/formulario_alta.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('./controlador/cMinijuegos.php');
-
-    $objCMinijuegos = new CMinijuego();
-    $ambitos = $objCMinijuegos->formularioAltaMostrar();
-    
-    include_once($objCMinijuegos->vista);
-?>
