0d4ac2a475cd899826bd16189a02fcd01c29fa9f
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-03-13 14:13:28 +0100

    Delete DWES/Ejercicios PHP/subida_archivos crud minijuegos directory

--- DWES/Ejercicios PHP/subida_archivos crud minijuegos/eliminarMinijuego.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("controladores/Cminijuego.php");
-    $objminijuego = new Cminijuego();
-    // Llama al método cBorrarMinijuego() para eliminar un minijuego
-    $resultado = $objminijuego->cBorrarMinijuego();   
-    header("Location: ".$objminijuego->vista.".php");
-    // Redirige a la vista después de eliminar el minijuego
-?>
