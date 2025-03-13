4d18c87c982b11f89be39923fa626656d67a70be
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-03-13 17:04:45 +0100

    Add files via upload

---
+++ DWES/Ejercicios PHP/Subida Archivos v3/procesar-imagen.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_FILES['imagen'])) {
+    // Extensiones permitidas
+    $extensiones_permitidas = ['image/jpeg', 'image/png'];
+
+    if (in_array($_FILES['imagen']['type'], $extensiones_permitidas)) {
+        $directorio_subida = 'archivos/';
+        $ruta_destino = $directorio_subida . basename($_FILES['imagen']['name']);
+
+        if (move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta_destino)) {
+            echo "<p>Imagen subida correctamente.</p>";
+        } else {
+            echo "<p>Error al subir la imagen.</p>";
+        }
+    } else {
+        echo "<p>Formato no permitido. Solo se aceptan imágenes JPG y PNG.</p>";
+    }
+} else {
+    echo "<p>No se ha seleccionado ningún archivo.</p>";
+}
+?>
