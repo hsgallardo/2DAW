e0edacaee9d40bf52963e44d2315e68d44d87a96
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-05-06 12:49:32 +0200

    Delete 3Evaluación/TAR-04.02 Consultas Varias Tablas/6.sql

--- 3Evaluación/TAR-04.02 Consultas Varias Tablas/6.sql
+++
@@ -1,8 +0,0 @@
-SELECT Lugares.lugar, Jesuitas.nombre
-FROM Lugares
-LEFT JOIN Visita ON Lugares.ip = Visita.ip
-LEFT JOIN Jesuitas ON Visita.idJesuita = Jesuitas.idJesuita;
-
--- 6- Muestra todos los lugares con el nombre del jesuita que realiza la visitada. 
--- Si algún lugar no se ha visitado, también habrá que visualizar sus datos ´mira que ocurre con el
--- valor del campo nombre (jesuita) en estos casos.
