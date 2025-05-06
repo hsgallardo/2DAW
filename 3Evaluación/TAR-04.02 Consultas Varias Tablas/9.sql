bc3c8556a9496abb9796f058e67af1e76c42000d
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-05-06 12:50:00 +0200

    Delete 3Evaluación/TAR-04.02 Consultas Varias Tablas/9.sql

--- 3Evaluación/TAR-04.02 Consultas Varias Tablas/9.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT Jesuitas.nombre
-FROM Jesuitas
-INNER JOIN Visita ON Jesuitas.idJesuita = Visita.idJesuita;
-
--- 9- Muestra el nombre de los jesuitas que han realizado alguna 
--- visitas (no hay que mostrar ningún dato más de la visita).
