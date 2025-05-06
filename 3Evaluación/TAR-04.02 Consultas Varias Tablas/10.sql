723af07ce5eb6cb0f48930377d94ad75afdc5005
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2025-05-06 12:48:06 +0200

    Delete 3Evaluación/TAR-04.02 Consultas Varias Tablas/10.sql

--- 3Evaluación/TAR-04.02 Consultas Varias Tablas/10.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT Lugares.lugar
-FROM Lugares
-INNER JOIN Visita ON Lugares.ip = Visita.ip;
-
-
--- 10- Piensa otra consulta diferente con DISTINCT (con la misma base de datos).
---     --10.1 Muestra los lugares distintos que han sido visitados, es decir, sin repetir los lugares.
